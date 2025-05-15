2b93527491256f2807df8e8d59ccf61b2d21c86f
guy33liba <guy33liba@gmail.com>
2025-05-15 04:51:08 +0300

    afdsa

---
+++ backend/database/factories/ContactFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ContactFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'phone' => $this->faker->phoneNumber(),
+            'notes' => $this->faker->sentence(),
+            'created_at' => now(),
+        ];
+    }
+}
